2366d8184b8b89df6c515e7bdc6284ed8419c18e
Phanupong-M <panupong38240@gmail.com>
2025-03-14 15:45:00 +0700

    feat: responsive search bar in article section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar.jsx';
-import HeroSection from './components/HeroSection.jsx'; 
+import HeroSection from './components/HeroSection.jsx';
+import ArticleSection from './components/ArticleSection.jsx';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <HeroSection />
+      <ArticleSection />
     </div>
   );
 }
