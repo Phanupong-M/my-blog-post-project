b5f9db71c98f3ff4f8542435203653db3ea7d50c
Phanupong-M <panupong38240@gmail.com>
2025-03-14 15:53:31 +0700

    feat: add reponsive footer section for mobile/desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from './components/Navbar.jsx';
 import HeroSection from './components/HeroSection.jsx'; 
-
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <Footer />
     </div>
   );
 }
