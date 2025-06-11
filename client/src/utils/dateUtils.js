4bd232c9c0fe6e97d1c008f9c0d0a6b7ac6695d4
Phanupong-M <panupong38240@gmail.com>
2025-06-11 20:40:44 +0700

    fix: update date to show time

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -1,7 +1,13 @@
-import { format } from "date-fns";
-
 export const formatDate = (isoDate) => {
   if (!isoDate) return "";
   const date = new Date(isoDate);
-  return format(date, "dd MMMM yyyy");
-}; 
+  return date.toLocaleString("en-GB", {
+    timeZone: "Asia/Bangkok",
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+};
